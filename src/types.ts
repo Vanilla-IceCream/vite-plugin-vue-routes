f80819793febdb590f24e7abd7f1206c0a4b94cc
Shyam-Chen <shyamchen1994@gmail.com>
2023-06-30 16:42:32 +0800

    26th Commit

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,36 @@
 export interface PluginOptions {
   /**
-   * Directory of the `routes`. Defaults to `src/routes`.
+   * Directory of the routes.
+   *
+   * @default 'src/routes'
    */
   routesDir?: string;
+
+  /**
+   * Directory of the layouts.
+   *
+   * @default 'src/layouts'
+   */
+  layoutsDir?: string;
+
+  /**
+   * Directory of the middleware.
+   *
+   * @default 'src/middleware'
+   */
+  middlewareDir?: string;
+
+  /**
+   * File of the route definition.
+   *
+   * @default 'Registry.vue'
+   */
+  routeFile?: string;
+
+  /**
+   * Macro of the route definition for layouts and middleware.
+   *
+   * @default 'defineRegistry'
+   */
+  routeMacro?: string;
 }
