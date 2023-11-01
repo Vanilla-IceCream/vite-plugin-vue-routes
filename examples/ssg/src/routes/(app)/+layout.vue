9bd5e4d45b7fe34250230061a8d867910d62415a
Shyam-Chen <shyamchen1994@gmail.com>
2023-11-01 20:51:26 +0800

    39th Commit

--- examples/ssg/src/routes/(app)/+layout.vue
+++ examples/ssg/src/routes/(app)/+layout.vue
@@ -1,15 +1,10 @@
 <script lang="ts" setup>
-import { RouterLink, RouterView } from 'vue-router';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
   <fieldset>
     <legend>/(app)/+layout.vue</legend>
-
-    <ul>
-      <li><RouterLink to="/dashboard">Dashboard</RouterLink></li>
-      <li><RouterLink to="/admin">Admin</RouterLink></li>
-    </ul>
 
     <RouterView />
   </fieldset>
--- examples/ssg/src/routes/(marketing)/+layout.vue
+++ examples/ssg/src/routes/(marketing)/+layout.vue
@@ -1,15 +1,10 @@
 <script lang="ts" setup>
-import { RouterLink, RouterView } from 'vue-router';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
   <fieldset>
     <legend>/(marketing)/+layout.vue</legend>
-
-    <ul>
-      <li><RouterLink to="/">Home</RouterLink></li>
-      <li><RouterLink to="/about">About</RouterLink></li>
-    </ul>
 
     <RouterView />
   </fieldset>
