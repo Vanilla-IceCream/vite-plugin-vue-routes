0c25b74e229a0cdbcd0a83e607ae248ec1641c0b
Shyam-Chen <shyamchen1994@gmail.com>
2024-01-28 15:12:31 +0800

    42nd Commit

--- src/__tests__/generateRoutes.spec.ts
+++ src/__tests__/generateRoutes.spec.ts
@@ -11,22 +11,22 @@ test('basic', async () => {
   expect(routes).toMatchInlineSnapshot(`
     "export default [
       {
-        \\"path\\": \\"/+\\",
-        \\"component\\": () => import('~/routes/+layout.vue'),
-        \\"children\\": [
+        "path": "/+",
+        "component": () => import('~/routes/+layout.vue'),
+        "children": [
           {
-            \\"path\\": \\"/about\\",
-            \\"component\\": () => import('~/routes/about/+page.vue')
+            "path": "/about",
+            "component": () => import('~/routes/about/+page.vue')
           },
           {
-            \\"path\\": \\"/\\",
-            \\"component\\": () => import('~/routes/(home)/+page.vue')
+            "path": "/",
+            "component": () => import('~/routes/(home)/+page.vue')
           }
         ]
       },
       {
-        \\"path\\": \\"/:slug(.*)*\\",
-        \\"component\\": () => import('~/routes/+error.vue')
+        "path": "/:slug(.*)*",
+        "component": () => import('~/routes/+error.vue')
       }
     ];"
   `);
@@ -40,48 +40,52 @@ test('layouts', async () => {
   expect(routes).toMatchInlineSnapshot(`
     "export default [
       {
-        \\"path\\": \\"/+\\",
-        \\"component\\": () => import('~/routes/+layout.vue'),
-        \\"children\\": [
+        "path": "/+",
+        "component": () => import('~/routes/+layout.vue'),
+        "children": [
           {
-            \\"path\\": \\"/layout-page/+\\",
-            \\"component\\": () => import('~/routes/layout-page/+layout.vue'),
-            \\"children\\": [
+            "path": "/layout-page/+",
+            "component": () => import('~/routes/layout-page/+layout.vue'),
+            "children": [
               {
-                \\"path\\": \\"/layout-page\\",
-                \\"component\\": () => import('~/routes/layout-page/+page.vue')
+                "path": "/layout-page",
+                "component": () => import('~/routes/layout-page/+page.vue')
               }
             ]
           },
           {
-            \\"path\\": \\"/+\\",
-            \\"component\\": () => import('~/routes/(marketing)/+layout.vue'),
-            \\"children\\": [
+            "path": "/+",
+            "component": () => import('~/routes/(app)/+layout.vue'),
+            "children": [
               {
-                \\"path\\": \\"/about\\",
-                \\"component\\": () => import('~/routes/(marketing)/about/+page.vue')
+                "path": "/dashboard",
+                "component": () => import('~/routes/(app)/dashboard/+page.vue')
               },
               {
-                \\"path\\": \\"/\\",
-                \\"component\\": () => import('~/routes/(marketing)/(home)/+page.vue')
+                "path": "/admin",
+                "component": () => import('~/routes/(app)/admin/+page.vue')
               }
             ]
           },
           {
-            \\"path\\": \\"/+\\",
-            \\"component\\": () => import('~/routes/(app)/+layout.vue'),
-            \\"children\\": [
+            "path": "/+",
+            "component": () => import('~/routes/(marketing)/+layout.vue'),
+            "children": [
               {
-                \\"path\\": \\"/dashboard\\",
-                \\"component\\": () => import('~/routes/(app)/dashboard/+page.vue')
+                "path": "/about",
+                "component": () => import('~/routes/(marketing)/about/+page.vue')
               },
               {
-                \\"path\\": \\"/admin\\",
-                \\"component\\": () => import('~/routes/(app)/admin/+page.vue')
+                "path": "/",
+                "component": () => import('~/routes/(marketing)/(home)/+page.vue')
               }
             ]
           }
         ]
+      },
+      {
+        "path": "/:slug(.*)*",
+        "component": () => import('~/routes/+error.vue')
       }
     ];"
   `);
@@ -95,76 +99,80 @@ test('nested', async () => {
   expect(routes).toMatchInlineSnapshot(`
     "export default [
       {
-        \\"path\\": \\"/+\\",
-        \\"component\\": () => import('~/routes/+layout.vue'),
-        \\"children\\": [
+        "path": "/+",
+        "component": () => import('~/routes/+layout.vue'),
+        "children": [
           {
-            \\"path\\": \\"/+\\",
-            \\"component\\": () => import('~/routes/(marketing)/+layout.vue'),
-            \\"children\\": [
+            "path": "/layout-page/+",
+            "component": () => import('~/routes/layout-page/+layout.vue'),
+            "children": [
               {
-                \\"path\\": \\"/\\",
-                \\"component\\": () => import('~/routes/(marketing)/(home)/+page.vue')
+                "path": "/layout-page",
+                "component": () => import('~/routes/layout-page/+page.vue')
+              }
+            ]
+          },
+          {
+            "path": "/+",
+            "component": () => import('~/routes/(marketing)/+layout.vue'),
+            "children": [
+              {
+                "path": "/about",
+                "component": () => import('~/routes/(marketing)/about/+page.vue')
               },
               {
-                \\"path\\": \\"/about\\",
-                \\"component\\": () => import('~/routes/(marketing)/about/+page.vue')
+                "path": "/",
+                "component": () => import('~/routes/(marketing)/(home)/+page.vue')
               }
             ]
           },
           {
-            \\"path\\": \\"/layout-page/+\\",
-            \\"component\\": () => import('~/routes/layout-page/+layout.vue'),
-            \\"children\\": [
+            "path": "/+",
+            "component": () => import('~/routes/(app)/+layout.vue'),
+            "children": [
               {
-                \\"path\\": \\"/layout-page\\",
-                \\"component\\": () => import('~/routes/layout-page/+page.vue')
-              }
-            ]
-          },
-          {
-            \\"path\\": \\"/+\\",
-            \\"component\\": () => import('~/routes/(app)/+layout.vue'),
-            \\"children\\": [
-              {
-                \\"path\\": \\"/admin/+\\",
-                \\"component\\": () => import('~/routes/(app)/admin/+layout.vue'),
-                \\"children\\": [
+                "path": "/admin/+",
+                "component": () => import('~/routes/(app)/admin/+layout.vue'),
+                "children": [
                   {
-                    \\"path\\": \\"/admin\\",
-                    \\"component\\": () => import('~/routes/(app)/admin/+page.vue')
+                    "path": "/admin",
+                    "component": () => import('~/routes/(app)/admin/+page.vue')
                   },
                   {
-                    \\"path\\": \\"/admin/theme\\",
-                    \\"component\\": () => import('~/routes/(app)/admin/theme/+page.vue')
+                    "path": "/admin/theme",
+                    "component": () => import('~/routes/(app)/admin/theme/+page.vue')
                   }
                 ]
               },
               {
-                \\"path\\": \\"/dashboard\\",
-                \\"component\\": () => import('~/routes/(app)/dashboard/+page.vue')
+                "path": "/dashboard",
+                "component": () => import('~/routes/(app)/dashboard/+page.vue')
               }
             ]
           },
           {
-            \\"path\\": \\"/users/:username/+\\",
-            \\"component\\": () => import('~/routes/users/[username]/+layout.vue'),
-            \\"children\\": [
+            "path": "/users/:username/+",
+            "component": () => import('~/routes/users/[username]/+layout.vue'),
+            "children": [
               {
-                \\"path\\": \\"/users/:username/profile\\",
-                \\"component\\": () => import('~/routes/users/[username]/profile/+page.vue')
+                "path": "/users/:username/profile",
+                "component": () => import('~/routes/users/[username]/profile/+page.vue')
               },
               {
-                \\"path\\": \\"/users/:username\\",
-                \\"component\\": () => import('~/routes/users/[username]/(home)/+page.vue')
+                "path": "/users/:username/posts",
+                "component": () => import('~/routes/users/[username]/posts/+page.vue')
               },
               {
-                \\"path\\": \\"/users/:username/posts\\",
-                \\"component\\": () => import('~/routes/users/[username]/posts/+page.vue')
+                "path": "/users/:username",
+                "component": () => import('~/routes/users/[username]/(home)/+page.vue')
               }
             ]
           }
         ]
+      },
+      {
+        "path": "/:slug(.*)*",
+        "component": () => import('~/routes/+error.vue')
       }
     ];"
   `);
@@ -178,58 +186,62 @@ test('params', async () => {
   expect(routes).toMatchInlineSnapshot(`
     "export default [
       {
-        \\"path\\": \\"/products\\",
-        \\"component\\": () => import('~/routes/products/+page.vue')
+        "path": "/products",
+        "component": () => import('~/routes/products/+page.vue')
       },
       {
-        \\"path\\": \\"/hello-world\\",
-        \\"component\\": () => import('~/routes/hello-world/+page.vue')
+        "path": "/hello-world",
+        "component": () => import('~/routes/hello-world/+page.vue')
       },
       {
-        \\"path\\": \\"/dashboard\\",
-        \\"component\\": () => import('~/routes/dashboard/+page.vue')
+        "path": "/dashboard",
+        "component": () => import('~/routes/dashboard/+page.vue')
       },
       {
-        \\"path\\": \\"/\\",
-        \\"component\\": () => import('~/routes/(home)/+page.vue')
+        "path": "/",
+        "component": () => import('~/routes/(home)/+page.vue')
       },
       {
-        \\"path\\": \\"/posts/:title?\\",
-        \\"component\\": () => import('~/routes/posts/[[title]]/+page.vue')
+        "path": "/posts/:title?",
+        "component": () => import('~/routes/posts/[[title]]/+page.vue')
       },
       {
-        \\"path\\": \\"/products/:id\\",
-        \\"component\\": () => import('~/routes/products/[id]/+page.vue')
+        "path": "/products/:id",
+        "component": () => import('~/routes/products/[id]/+page.vue')
       },
       {
-        \\"path\\": \\"/blog/:info*\\",
-        \\"component\\": () => import('~/routes/blog/[...info]/+page.vue')
+        "path": "/blog/:info*",
+        "component": () => import('~/routes/blog/[...info]/+page.vue')
       },
       {
-        \\"path\\": \\"/bar\\",
-        \\"component\\": () => import('~/routes/(group)/bar/+page.vue')
+        "path": "/foo",
+        "component": () => import('~/routes/(group)/foo/+page.vue')
       },
       {
-        \\"path\\": \\"/foo\\",
-        \\"component\\": () => import('~/routes/(group)/foo/+page.vue')
+        "path": "/bar",
+        "component": () => import('~/routes/(group)/bar/+page.vue')
       },
       {
-        \\"path\\": \\"/users/:username/+\\",
-        \\"component\\": () => import('~/routes/users/[username]/+layout.vue'),
-        \\"children\\": [
+        "path": "/users/:username/+",
+        "component": () => import('~/routes/users/[username]/+layout.vue'),
+        "children": [
           {
-            \\"path\\": \\"/users/:username/profile\\",
-            \\"component\\": () => import('~/routes/users/[username]/profile/+page.vue')
+            "path": "/users/:username/profile",
+            "component": () => import('~/routes/users/[username]/profile/+page.vue')
           },
           {
-            \\"path\\": \\"/users/:username/posts\\",
-            \\"component\\": () => import('~/routes/users/[username]/posts/+page.vue')
+            "path": "/users/:username/posts",
+            "component": () => import('~/routes/users/[username]/posts/+page.vue')
           },
           {
-            \\"path\\": \\"/users/:username\\",
-            \\"component\\": () => import('~/routes/users/[username]/(home)/+page.vue')
+            "path": "/users/:username",
+            "component": () => import('~/routes/users/[username]/(home)/+page.vue')
           }
         ]
+      },
+      {
+        "path": "/:slug(.*)*",
+        "component": () => import('~/routes/+error.vue')
       }
     ];"
   `);
